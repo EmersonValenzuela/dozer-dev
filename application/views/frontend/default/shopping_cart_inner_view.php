43f0832dd9868a56ca8035d301dcb21ab6cdb314
Emerson Valenzuela <52836406+EmersonValenzuela@users.noreply.github.com>
2023-05-09 17:00:41 -0500

    is

--- application/views/frontend/default/shopping_cart_inner_view.php
+++ application/views/frontend/default/shopping_cart_inner_view.php
@@ -3,7 +3,7 @@
         <div class="fondo-gqp">
             <div class="title text-carrito  py-3">Tu carrito de compra esta vacío</div>
             <div class="hidden"><?php echo sizeof($this->session->userdata('cart_items')) . '  ' ?></div>
-            <div><img class="d-block m-auto" src="<?=base_url()?>uploads/system/carrito.png" alt=""></div>
+            <div><img class="d-block m-auto" src="<?= base_url() ?>uploads/system/carrito.png" alt=""></div>
             <div class="d-flex flex-column mb-3 p-2">
                 <div class="py-2  px-4"><a class=" fondo-comprar-ced d-flex justify-content-center" href="#">Seguir
                         comprando cursos</a></div>
@@ -23,64 +23,62 @@
                     $course_details = $this->crud_model->get_course_by_id($cart_item)->row_array();
                     $instructor_details = $this->user_model->get_all_user($course_details['user_id'])->row_array();
                 ?>
-                <li>
-                    <div class="cart-course-wrapper box-shadow-5 d-flex justify-content-between">
+                    <li>
+                        <div class="cart-course-wrapper box-shadow-5 d-flex justify-content-between">
 
-                        <div class="d-flex flex-column ">
-                            <div class="d-flex flex-row justify-content-between ">
-                                <div class="details">
-                                    <a
-                                        href="<?php echo site_url('home/course/' . rawurlencode(slugify($course_details['title'])) . '/' . $course_details['id']); ?>">
-                                        <div class="name text-title-cqp text-white">
-                                            <?php echo $course_details['title']; ?>
+                            <div class="d-flex flex-column ">
+                                <div class="d-flex flex-row justify-content-between ">
+                                    <div class="details">
+                                        <a href="<?php echo site_url('home/course/' . rawurlencode(slugify($course_details['title'])) . '/' . $course_details['id']); ?>">
+                                            <div class="name text-title-cqp text-white">
+                                                <?php echo $course_details['title']; ?>
+                                            </div>
+                                        </a>
+
+                                        <div class="course-subtitle text-13px mt-2">
+                                            <?php echo ellipsis($course_details['short_description'], 80); ?>
                                         </div>
-                                    </a>
 
-                                    <div class="course-subtitle text-13px mt-2">
-                                        <?php echo ellipsis($course_details['short_description'], 80); ?>
+
                                     </div>
 
 
+                                    <div class="move-remove text-center ">
+                                        <div id="<?php echo $course_details['id']; ?>" onclick="removeFromCartList(this)"><i style="font-size: 30px;" class="fas fa-times-circle text-white"></i></div>
+                                        <!-- <div>Move to Wishlist</div> -->
+                                    </div>
                                 </div>
+                                <hr class="stylehr">
+                                <div class="price mt-1 mx-3">
+                                    <?php if ($course_details['discount_flag'] == 1) : ?>
+                                        <div class="current-price text-white d-flex flex-row  justify-content-between">
+                                            <div>Costo:
+                                            </div>
+                                            <div class="">
+                                                <?php
+                                                $total_price += $course_details['discounted_price'];
+                                                echo currency($course_details['discounted_price']);
+                                                ?>
+                                            </div>
+                                        </div>
 
 
-                                <div class="move-remove text-center ">
-                                    <div id="<?php echo $course_details['id']; ?>" onclick="removeFromCartList(this)"><i
-                                            style="font-size: 30px;" class="fas fa-times-circle text-white"></i></div>
-                                    <!-- <div>Move to Wishlist</div> -->
+                                    <?php else : ?>
+
+
+                                        <div class="current-price">
+                                            <?php
+                                            $actual_price += $course_details['price'];
+                                            $total_price  += $course_details['price'];
+                                            echo currency($course_details['price']);
+                                            ?>
+                                        </div>
+                                    <?php endif; ?>
                                 </div>
+                                <hr class="stylehr">
                             </div>
-                            <hr class="stylehr">
-                            <div class="price mt-1 mx-3">
-                                <?php if ($course_details['discount_flag'] == 1) : ?>
-                                <div class="current-price text-white d-flex flex-row  justify-content-between">
-                                    <div>Costo:
-                                    </div>
-                                    <div class="">
-                                        <?php
-                                        $total_price += $course_details['discounted_price'];
-                                        echo currency($course_details['discounted_price']);
-                                        ?>
-                                    </div>
-                                </div>
-
-
-                                <?php else : ?>
-                                
-
-                                <div class="current-price">
-                                    <?php
-                                        $actual_price += $course_details['price'];
-                                        $total_price  += $course_details['price'];
-                                        echo currency($course_details['price']);
-                                        ?>
-                                </div>
-                                <?php endif; ?>
-                            </div>
-                            <hr class="stylehr">
                         </div>
-                    </div>
-                </li>
+                    </li>
                 <?php endforeach; ?>
             </ul>
         </div>
@@ -88,18 +86,18 @@
     <h5 class="fw-700"><?php echo site_phrase('total'); ?>:</h5>
     <div class="cart-sidebar bg-white radius-10 py-4 px-3 box-shadow-5">
         <?php if (isset($coupon_code) && !empty($coupon_code) && $this->crud_model->check_coupon_validity($coupon_code)) : ?>
-        <span id="total_price_of_checking_out" hidden>
-            <?php
+            <span id="total_price_of_checking_out" hidden>
+                <?php
                 $coupon_details = $this->crud_model->get_coupon_details_by_code($coupon_code)->row_array();
                 $actual_price = round($total_price, 2);
-                
+
                 //including Tax or vat
-                if(get_settings('course_selling_tax') > 0):
+                if (get_settings('course_selling_tax') > 0) :
                     $total_price = $this->crud_model->get_discounted_price_after_applying_coupon($coupon_code);
 
-                    $total_tax = round(($total_price/100) * get_settings('course_selling_tax'), 2);
-                    $total_price = round($total_price + ($total_price/100) * get_settings('course_selling_tax'), 2);
-                else:
+                    $total_tax = round(($total_price / 100) * get_settings('course_selling_tax'), 2);
+                    $total_price = round($total_price + ($total_price / 100) * get_settings('course_selling_tax'), 2);
+                else :
                     $total_price = round($this->crud_model->get_discounted_price_after_applying_coupon($coupon_code), 2);
                 endif;
 
@@ -108,52 +106,50 @@
                 $this->session->set_userdata('total_price_of_checking_out', $total_price);
                 $this->session->set_userdata('applied_coupon', $coupon_code);
                 ?>
-        </span>
-        <div class="total-price"><?php echo currency($total_price); ?></div>
-        <div class="total-original-price">
-            <span class="original-price">
-                <span class="original-price"><?php echo currency($actual_price); ?></span>
             </span>
-            <span class="discount-rate"><?php echo $coupon_details['discount_percentage']; ?>%
-                <?php echo site_phrase('coupon_code_applied'); ?></span>
-        </div>
+            <div class="total-price"><?php echo currency($total_price); ?></div>
+            <div class="total-original-price">
+                <span class="original-price">
+                    <span class="original-price"><?php echo currency($actual_price); ?></span>
+                </span>
+                <span class="discount-rate"><?php echo $coupon_details['discount_percentage']; ?>%
+                    <?php echo site_phrase('coupon_code_applied'); ?></span>
+            </div>
         <?php else : ?>
-        <!-- including Tax or vat -->
-        <?php
-                if(get_settings('course_selling_tax') > 0):
-                    $total_tax = round(($total_price/100) * get_settings('course_selling_tax'), 2);
-                    $total_price = round($total_price + ($total_price/100) * get_settings('course_selling_tax'), 2);
-                else:
-                    $total_price = round($total_price, 2);
-                endif;
+            <!-- including Tax or vat -->
+            <?php
+            if (get_settings('course_selling_tax') > 0) :
+                $total_tax = round(($total_price / 100) * get_settings('course_selling_tax'), 2);
+                $total_price = round($total_price + ($total_price / 100) * get_settings('course_selling_tax'), 2);
+            else :
+                $total_price = round($total_price, 2);
+            endif;
             ?>
 
-        <span id="total_price_of_checking_out" hidden><?php echo $total_price;
-                $this->session->set_userdata('total_price_of_checking_out', $total_price); ?>
-        </span>
-        <div class="total-price"><?php echo currency($total_price); ?></div>
-        <div class="total-original-price">
+            <span id="total_price_of_checking_out" hidden><?php echo $total_price;
+                                                            $this->session->set_userdata('total_price_of_checking_out', $total_price); ?>
+            </span>
+            <div class="total-price"><?php echo currency($total_price); ?></div>
+            <div class="total-original-price">
 
-            <!-- <span class="discount-rate">95% off</span> -->
-        </div>
+                <!-- <span class="discount-rate">95% off</span> -->
+            </div>
         <?php endif; ?>
 
 
         <!-- including Tax or vat -->
-        <?php if(get_settings('course_selling_tax') > 0): ?>
-        <small
-            class="d-block mb-3 w-100 text-12px text-uppercase"><b><?php echo currency($total_tax).'</b> ('.get_settings('course_selling_tax').'%) '.get_phrase('TAX_INCLUDED'); ?></small>
+        <?php if (get_settings('course_selling_tax') > 0) : ?>
+            <small class="d-block mb-3 w-100 text-12px text-uppercase"><b><?php echo currency($total_tax) . '</b> (' . get_settings('course_selling_tax') . '%) ' . get_phrase('TAX_INCLUDED'); ?></small>
         <?php endif; ?>
 
 
         <!-- including Tax or vat -->
-        <?php if(get_settings('course_selling_tax') > 0):
-            $total_price = $total_price + ($total_price/100) * get_settings('course_selling_tax');
-        endif;?>
+        <?php if (get_settings('course_selling_tax') > 0) :
+            $total_price = $total_price + ($total_price / 100) * get_settings('course_selling_tax');
+        endif; ?>
 
         <div class="input-group marge-input-box mb-3">
-            <input type="text" class="form-control" placeholder="<?php echo site_phrase('apply_coupon_code'); ?>"
-                id="coupon-code">
+            <input type="text" class="form-control" placeholder="<?php echo site_phrase('apply_coupon_code'); ?>" id="coupon-code">
             <div class="input-group-append">
                 <button class="btn" type="button" onclick="applyCoupon()">
                     <i class="fas fa-spinner fa-pulse hidden" id="spinner"></i>
@@ -161,7 +157,10 @@
                 </button>
             </div>
         </div>
-        <button type="button" class="btn red w-100 radius-10 mb-3"
-            onclick="handleCheckOut()"><?php echo site_phrase('checkout'); ?></button>
+        <button type="button" class="btn red w-100 radius-10 mb-3" onclick="handleCheckOut()"><?php echo site_phrase('checkout'); ?></button>
     </div>
 </div>
+
+<script>
+    console.log(<?php var_dump($this->session->userdata('cart_items')) ?>);
+</script>

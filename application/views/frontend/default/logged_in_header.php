7bbd16fa607b7e34207cd6a27f9232c881057c59
GiuliHr <ale.hr.27@hotmail.com>
2023-05-18 00:17:28 -0500

    navbar in

--- application/views/frontend/default/logged_in_header.php
+++ application/views/frontend/default/logged_in_header.php
@@ -19,39 +19,6 @@ $user_details = $this->user_model->get_user($this->session->userdata('user_id'))
 
 
 
-            <div class="me-auto d-none d-md-none d-lg-block text-white">
-
-                <li class="nav-item dropdown" style="list-style-type: none !important">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <?php
-                $categoria = $this->crud_model->get_categoria()->result_array();
-                foreach ($categoria as $key => $category):?>
-                <a class="text-white fw-500 px-2" href="javascript:;"
-                    onclick="redirect_to('<?php echo site_url('home/courses?category='.$category['slug']); ?>')">
-                    <span><?php echo $category['name']; ?></span>
-                    <span class="has-sub-category ms-auto"></span> </a>
-
-                <?php endforeach; ?>
-
-
-
-
-
-
-
-            </div>
 
            
 

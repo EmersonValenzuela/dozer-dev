25d46c7e48a88be42147c84f895a39fd79980ed9
GiuliHr <ale.hr.27@hotmail.com>
2023-09-06 00:13:16 -0500

    pagina Certificado

--- application/views/frontend/default/certificado.php
+++ application/views/frontend/default/certificado.php
@@ -6,15 +6,23 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-12">
                         <h1 class="text-center mt-4 text-section">¡Verifica tus Certificados!</h1>
-                        <p class="text-center mt-4">Buscar por documento de identidad</p>
+
+                        <div class="d-flex flex-row mb-3 align-items-center justify-content-center">
+                            <div class=""><p class="text-center mt-4">Buscar por documento de identidad:</p></div>
+                            <div class=" px-5">  <input class="bg-input" type="text" placeholder="Puedes usar tu DNI - CI"></div>
+                            <div class=""> <a href="#" class="btn-verificar"> Verificar</a></div>
+                        </div>
+                        
+                      
+                       
                     </div>
                     <div class="col-lg-3"></div><br>
                     <div class="row justify-content-center">
                         <div class="row text-white color-fondo  mb-3 ">
                             <div class="col-1 p-1 text-center">Formato</div>
-                            <div class="col-2 p-1 text-center">Institución</div>
+                            <div class="col-3 p-1 text-center">Institución</div>
                             <div class="col-3 p-1 text-center">Nombre del Estudiante</div>
-                            <div class="col-5 p-1 text-center">Nombre de la Capacitación</div>
+                            <div class="col-4 p-1 text-center">Nombre de la Capacitación</div>
                             <div class="col-1 p-1 text-center">Enlace</div>
                         </div>
                     </div>
@@ -22,11 +30,14 @@
                         <?php
                         if ($certificates) {
                             foreach ($certificates as $key => $certificate) : ?>
-                                <div class="col-1 col-xs-0 p-1 text-center"><?= $certificate->name ?></div>
-                                <div class="col-3 col-xs-0 p-1 text-center"><?= $certificate->institute ?></div>
-                                <div class="col-2 col-xs-0 p-1 text-center"><?= $certificate->last_name . " " . $certificate->first_name ?></div>
-                                <div class="col-5 col-xs-0 p-1 text-center"><?= $certificate->title ?></div>
-                                <div class="col-1 col-xs- 0xsp-1 text-center"><a class="text-danger" href="<?= base_url("uploads/certificates/" . $certificate->link) ?>" target="_blank">Enlace</a></div>
+                        <div class="col-1 col-xs-0 p-1 text-center"><?= $certificate->name ?></div>
+                        <div class="col-3 col-xs-0 p-1 text-center"><?= $certificate->institute ?></div>
+                        <div class="col-3 col-xs-0 p-1 text-center">
+                            <?= $certificate->last_name . " " . $certificate->first_name ?></div>
+                        <div class="col-4 col-xs-0 p-1 text-center"><?= $certificate->title ?></div>
+                        <div class="col-1 col-xs- 0xsp-1 text-center"><a class="text-danger"
+                                href="<?= base_url("uploads/certificates/" . $certificate->link) ?>"
+                                target="_blank">Enlace</a></div>
                         <?php endforeach;
                         } ?>
                     </div>

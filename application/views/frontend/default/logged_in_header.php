7168c844a0b07bee224c8db700af3ed254a79966
GiuliHr <34411540+Aghr123@users.noreply.github.com>
2023-10-23 16:38:20 -0500

    logged_in_header
    <div class="d-flex justify-content-end">...</div>

--- application/views/frontend/default/logged_in_header.php
+++ application/views/frontend/default/logged_in_header.php
@@ -32,20 +32,21 @@ $user_details = $this->user_model->get_user($this->session->userdata('user_id'))
                 <a class="navbar-brand btn-hover-gray text-14px ms-2 me-0 <?php if (isset($page_url) && $custom_page_menu['page_url'] == $page_url) echo 'active'; ?>" style="border: 1px solid transparent; margin: 0px; padding: 0px 8px; width: max-content; border-radius: 5px; height: 40px; line-height: 40px;" href="<?php echo site_url('page/' . $custom_page_menu['page_url']); ?>"><?php echo $custom_page_menu['button_title']; ?></a>
                 </li>
             <?php endforeach; ?>
-            <a class="mx-2 text-navbar-p main-nav-wrap" href=" <?php echo base_url('Premium') ?>">
-                <img class="icon-corona " src="<?= base_url() ?>uploads/system/corona-premium.svg" alt=""> Premium
-            </a>
+            <div class="d-flex justify-content-end">
+                <a class="mx-2 text-navbar-p main-nav-wrap" href=" <?php echo base_url('Premium') ?>">
+                    <img class="icon-corona " src="<?= base_url() ?>uploads/system/corona-premium.svg" alt=""> Premium
+                </a>
 
-            <?php if (get_settings('allow_instructor') == 1) : ?>
-                <div class="instructor-box menu-icon-box ms-md-3">
-                    <div class="icon">
-                        <a class="text-white" href="<?php echo site_url('user'); ?>" style="border: 1px solid transparent; margin: 0px; padding: 0px 10px; font-size: 14px; width: max-content; border-radius: 5px; height: 40px; line-height: 40px;"><?php echo site_phrase('instructor'); ?></a>
+                <?php if (get_settings('allow_instructor') == 1) : ?>
+                    <div class="instructor-box menu-icon-box ms-md-3">
+                        <div class="icon">
+                            <a class="text-white" href="<?php echo site_url('user'); ?>" style="border: 1px solid transparent; margin: 0px; padding: 0px 10px; font-size: 14px; width: max-content; border-radius: 5px; height: 40px; line-height: 40px;"><?php echo site_phrase('instructor'); ?></a>
+                        </div>
                     </div>
-                </div>
-            <?php endif;
-            ?>
+                <?php endif;
+                ?>
 
-            <!-- <div class="instructor-box menu-icon-box">
+                <!-- <div class="instructor-box menu-icon-box">
                 <div class="icon">
                     <a class="text-white" href="<?php echo site_url('home/my_courses'); ?>"
                         style="border: 1px solid transparent; margin: 0px; padding: 0px 10px; font-size: 14px; width: max-content; border-radius: 5px; height: 40px; line-height: 40px;"><?php echo site_phrase('my_courses'); ?></a>
@@ -54,91 +55,91 @@ $user_details = $this->user_model->get_user($this->session->userdata('user_id'))
 
 
 
-            <div class="cart-box menu-icon-box" id="cart_items">
-                <?php include 'cart_items.php'; ?>
-            </div>
-
-
-
-            <div class="user-box menu-icon-box">
-                <div class="icon">
-                    <a href="javascript:;">
-                        <img src="<?php echo $this->user_model->get_user_image_url($this->session->userdata('user_id')); ?>" alt="" class="img-fluid">
-                    </a>
+                <div class="cart-box menu-icon-box" id="cart_items">
+                    <?php include 'cart_items.php'; ?>
                 </div>
-                <div class="dropdown user-dropdown corner-triangle top-right radius-10">
-                    <ul class="user-dropdown-menu radius-10">
 
-                        <li class="dropdown-user-info">
-                            <a href="javascript:;" class="radius-top-10">
-                                <div class="clearfix">
-                                    <div class="user-image float-start">
-                                        <img src="<?php echo $this->user_model->get_user_image_url($this->session->userdata('user_id')); ?>" alt="">
-                                    </div>
-                                    <div class="user-details">
-                                        <div class="user-name">
-                                            <span class="hi"><?php echo site_phrase('hi'); ?>,</span>
-                                            <?php echo $user_details['first_name'] . ' ' . $user_details['last_name']; ?>
+
+
+                <div class="user-box menu-icon-box">
+                    <div class="icon">
+                        <a href="javascript:;">
+                            <img src="<?php echo $this->user_model->get_user_image_url($this->session->userdata('user_id')); ?>" alt="" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="dropdown user-dropdown corner-triangle top-right radius-10">
+                        <ul class="user-dropdown-menu radius-10">
+
+                            <li class="dropdown-user-info">
+                                <a href="javascript:;" class="radius-top-10">
+                                    <div class="clearfix">
+                                        <div class="user-image float-start">
+                                            <img src="<?php echo $this->user_model->get_user_image_url($this->session->userdata('user_id')); ?>" alt="">
                                         </div>
-                                        <div class="user-email">
-                                            <span class="email"><?php echo $user_details['email']; ?></span>
-                                            <span class="welcome"><?php echo site_phrase("welcome_back"); ?></span>
+                                        <div class="user-details">
+                                            <div class="user-name">
+                                                <span class="hi"><?php echo site_phrase('hi'); ?>,</span>
+                                                <?php echo $user_details['first_name'] . ' ' . $user_details['last_name']; ?>
+                                            </div>
+                                            <div class="user-email">
+                                                <span class="email"><?php echo $user_details['email']; ?></span>
+                                                <span class="welcome"><?php echo site_phrase("welcome_back"); ?></span>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </a>
-                        </li>
+                                </a>
+                            </li>
 
-                        <li class="user-dropdown-menu-item"><a href="<?php echo site_url('home/my_courses'); ?>"><i class="far fa-gem"></i><?php echo site_phrase('my_courses'); ?></a></li>
-
-
-                        <li class="user-dropdown-menu-item"><a href="<?php echo site_url('home/purchase_history'); ?>"><i class="fas fa-shopping-cart"></i><?php echo site_phrase('purchase_history'); ?></a>
-                        </li>
-                        <li class="user-dropdown-menu-item"><a href="<?php echo site_url('home/profile/user_profile'); ?>"><i class="fas fa-user"></i><?php echo site_phrase('user_profile'); ?></a></li>
-                        <?php if (addon_status('affiliate_course')) :
-                            $CI    = &get_instance();
-                            $CI->load->model('addons/affiliate_course_model');
-                            $x = $CI->affiliate_course_model->is_affilator($this->session->userdata('user_id'));
-                            $is_affiliator = $CI->affiliate_course_model->is_affilator($this->session->userdata('user_id'));
-
-                            if ($x == 0 && get_settings('affiliate_addon_active_status') == 1) : ?>
+                            <li class="user-dropdown-menu-item"><a href="<?php echo site_url('home/my_courses'); ?>"><i class="far fa-gem"></i><?php echo site_phrase('my_courses'); ?></a></li>
 
 
-                                <li class="user-dropdown-menu-item"><a href="<?php echo site_url('addons/affiliate_course/become_an_affiliator'); ?>"><i class="fas fa-user-plus"></i><?php echo site_phrase('Become_an_Affiliator'); ?></a>
-                                </li>
-                            <?php else : ?>
-                                <?php if ($is_affiliator == 1) : ?>
+                            <li class="user-dropdown-menu-item"><a href="<?php echo site_url('home/purchase_history'); ?>"><i class="fas fa-shopping-cart"></i><?php echo site_phrase('purchase_history'); ?></a>
+                            </li>
+                            <li class="user-dropdown-menu-item"><a href="<?php echo site_url('home/profile/user_profile'); ?>"><i class="fas fa-user"></i><?php echo site_phrase('user_profile'); ?></a></li>
+                            <?php if (addon_status('affiliate_course')) :
+                                $CI    = &get_instance();
+                                $CI->load->model('addons/affiliate_course_model');
+                                $x = $CI->affiliate_course_model->is_affilator($this->session->userdata('user_id'));
+                                $is_affiliator = $CI->affiliate_course_model->is_affilator($this->session->userdata('user_id'));
+
+                                if ($x == 0 && get_settings('affiliate_addon_active_status') == 1) : ?>
 
 
-                                    <li class="user-dropdown-menu-item"><a href="<?php echo site_url('addons/affiliate_course/affiliate_course_history '); ?>"><i class="fa fa-book"></i><?php echo site_phrase('Affiliation History'); ?></a></li>
+                                    <li class="user-dropdown-menu-item"><a href="<?php echo site_url('addons/affiliate_course/become_an_affiliator'); ?>"><i class="fas fa-user-plus"></i><?php echo site_phrase('Become_an_Affiliator'); ?></a>
+                                    </li>
+                                <?php else : ?>
+                                    <?php if ($is_affiliator == 1) : ?>
+
+
+                                        <li class="user-dropdown-menu-item"><a href="<?php echo site_url('addons/affiliate_course/affiliate_course_history '); ?>"><i class="fa fa-book"></i><?php echo site_phrase('Affiliation History'); ?></a></li>
+                                    <?php endif; ?>
                                 <?php endif; ?>
                             <?php endif; ?>
-                        <?php endif; ?>
-                        <?php if (addon_status('customer_support')) : ?>
-                            <li class="user-dropdown-menu-item"><a href="<?php echo site_url('addons/customer_support/user_tickets'); ?>"><i class="fas fa-life-ring"></i><?php echo site_phrase('support'); ?></a></li>
-                        <?php endif; ?>
+                            <?php if (addon_status('customer_support')) : ?>
+                                <li class="user-dropdown-menu-item"><a href="<?php echo site_url('addons/customer_support/user_tickets'); ?>"><i class="fas fa-life-ring"></i><?php echo site_phrase('support'); ?></a></li>
+                            <?php endif; ?>
 
-                        <li class="dropdown-user-logout user-dropdown-menu-item radius-bottom-10"><a class="radius-bottom-10 py-3" href="<?php echo site_url('login/logout'); ?>"><i class="fas fa-sign-out-alt"></i> <?php echo site_phrase('log_out'); ?></a></li>
-                    </ul>
+                            <li class="dropdown-user-logout user-dropdown-menu-item radius-bottom-10"><a class="radius-bottom-10 py-3" href="<?php echo site_url('login/logout'); ?>"><i class="fas fa-sign-out-alt"></i> <?php echo site_phrase('log_out'); ?></a></li>
+                        </ul>
+                    </div>
                 </div>
+
+                <?php
+                if (site_url('user') != "") {
+                ?>
+
+                    <span class="signin-box-move-desktop-helper"></span>
+                    <div class="sign-in-box btn-group d-none">
+
+                        <button type="button" class="btn btn-sign-in" data-toggle="modal" data-target="#signInModal">Log
+                            In</button>
+
+                        <button type="button" class="btn btn-sign-up" data-toggle="modal" data-target="#signUpModal">Sign
+                            Up</button>
+
+                    </div> <!--  sign-in-box end -->
+                <?php } ?>
             </div>
-
-            <?php
-            if (site_url('user') != "") {
-            ?>
-
-                <span class="signin-box-move-desktop-helper"></span>
-                <div class="sign-in-box btn-group d-none">
-
-                    <button type="button" class="btn btn-sign-in" data-toggle="modal" data-target="#signInModal">Log
-                        In</button>
-
-                    <button type="button" class="btn btn-sign-up" data-toggle="modal" data-target="#signUpModal">Sign
-                        Up</button>
-
-                </div> <!--  sign-in-box end -->
-            <?php } ?>
-
         </nav>
     </div>
 </section>
